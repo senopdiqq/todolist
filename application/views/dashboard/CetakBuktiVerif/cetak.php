63745112dba2c479c12766fac80fb18a9b51b0a0
Yudas <yudasmalabi@gmail.com>
2021-03-25 00:45:34 +0700

    done cetak bukti verifikasi

--- application/views/dashboard/CetakBuktiVerif/cetak.php
+++ application/views/dashboard/CetakBuktiVerif/cetak.php
@@ -80,10 +80,150 @@
                     </tr>
                     <tr>
                         <td>
+                            <p class="text-dark ml-5">Desa</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-5">: <?= $cetak->nama_desa ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">RT / RW</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-5">: <?= $cetak->rt ?> / <?= $cetak->rw ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
                             <p class="text-dark ml-5">NIB</p>
                         </td>
                         <td>
-                            <p class="text-dark ml-5">: <?= $cetak->nib ?></p> :
+                            <p class="text-dark ml-5">: <?= $cetak->nib ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">Luas Tanah</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-5">: <?= $cetak->luas_tanah ?> m<sup style="right: 0;">2</sup></p>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="row mt-3 ml-5">
+            <table class="ml-10 mr-auto" border="0" align="left">
+                <thead>
+                    <tr>
+                        <td class="text-left">
+                            <p class="text-dark">2. Diterbitkan atas permohonan : </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">NIK</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-3">: <?= $cetak->nik ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">Nama Pemohon</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-3">: <?= $cetak->nama ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">Pekerjaan</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-3">: <?= $cetak->pekerjaan ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">Alamat</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-3">: <?= $cetak->alamat ?></p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="text-dark ml-5">Jenis Kelamin</p>
+                        </td>
+                        <td>
+                            <p class="text-dark ml-3">: <?= $cetak->jenis_kelamin ?></p>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="row mt-3 ml-5">
+            <table class="ml-10 mr-auto" border="0" align="left">
+                <thead>
+                    <tr>
+                        <td class="text-left">
+                            <p class="text-dark">3. Untuk Keperluan : <b>PERIZINAN</b> </p>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="row mt-3 ml-5">
+            <table class="ml-10 mr-auto" border="0" align="left">
+                <thead>
+                    <tr>
+                        <td class="text-left">
+                            <p class="text-dark">4. Surat Keterangan Pendaftaran Tanah ini bukan merupakan Tanda Bukti Hak Atas Tanah</p>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="row mt-8 ml-8">
+            <table class="ml-10 mr-auto" border="0" align="left">
+                <thead>
+                    <tr>
+                        <td class="text-left font-italic">
+                            <p class="text-dark">Informasi ini valid selama 7 hari kalender sejak diterbitkan</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left font-italic">
+                            <p class="text-dark">Diterbitkan pada tanggal <?= date('d M Y') ?> pukul <?= date('H:i:s') ?> </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left font-italic">
+                            <p class="text-dark">Lokasi bidang tanah dapat dicek melalui aplikasi 'Sentuh Tanahku'</p>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="row mt-3 ml-5">
+            <table class="ml-auto" border="0" align="right">
+                <thead>
+                    <tr>
+                        <td class="text-center">
+                            <p class="text-dark">Malang,<?= date('d M Y') ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">
+                            <p class="text-dark mt-6">_________________</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">
+                            <p class="text-dark mt-6"><?= $this->session->userdata('nama') ?></p>
                         </td>
                     </tr>
                 </thead>
@@ -94,38 +234,7 @@
 </body>
 
 </html>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div>
 
-
-    <br>
-    <div style="float:right;">
-        <p style="text-align:center">Malang,<?= date('d M Y') ?></p>
-        <br>
-        <br>
-        <br>
-        <p>_________</p>
-        <p style="text-align:center"><?= $this->session->userdata('nama') ?></p>
-    </div>
-</div>
+<script>
+    window.print();
+</script>
